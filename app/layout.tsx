491ee7dbc030aa1d927e0ad3d70165118e403667
Vicki <s6p1j@students.cs.ubc.ca>
2023-12-28 21:07:54 -0800

    login button setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,66 +1,16 @@
-// import "@/src/styles/globals.css"
-// import { Metadata } from "next"
+import "@/src/styles/globals.css"
+import { SiteHeader } from "@/src/components/site-header"
 
-// import { siteConfig } from "@/src/config/site"
-// import { fontSans } from "@/src/lib/fonts"
-// import { cn } from "@/src/lib/utils"
-// import { SiteHeader } from "@/src/components/site-header"
-// import { TailwindIndicator } from "@/src/components/tailwind-indicator"
-// import { ThemeProvider } from "@/src/components/theme-provider"
-
-// export const metadata: Metadata = {
-//   title: {
-//     default: siteConfig.name,
-//     template: `%s - ${siteConfig.name}`,
-//   },
-//   description: siteConfig.description,
-//   themeColor: [
-//     { media: "(prefers-color-scheme: light)", color: "white" },
-//     { media: "(prefers-color-scheme: dark)", color: "black" },
-//   ],
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-// }
-
-// interface RootLayoutProps {
-//   children: React.ReactNode
-// }
-
-// export default function RootLayout({ children }: RootLayoutProps) {
-//   return (
-//     <>
-//       <html lang="en" suppressHydrationWarning>
-//         <head />
-//         <body
-//           className={cn(
-//             "min-h-screen bg-background font-sans antialiased",
-//             fontSans.variable
-//           )}
-//         >
-//           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-//             <div className="relative flex min-h-screen flex-col">
-//               <SiteHeader />
-//               <div className="flex-1">{children}</div>
-//             </div>
-//             <TailwindIndicator />
-//           </ThemeProvider>
-//         </body>
-//       </html>
-//     </>
-//   )
-// }
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <div>
+          <SiteHeader />
+          <div>{children}</div>
+        </div>
+      </body>
+
     </html>
   )
 }
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,52 +1,17 @@
 import Link from "next/link"
-
-import { siteConfig } from "@/src/config/site"
-import { buttonVariants } from "@/src/components/ui/button"
-import { Icons } from "@/src/components/icons"
-import { MainNav } from "@/src/components/main-nav"
-import { ThemeToggle } from "@/src/components/theme-toggle"
+import styles from "@/src/styles/header.module.css"
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
-            <ThemeToggle />
-          </nav>
-        </div>
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <h1>logo</h1>
+      </div>
+      <div className={styles.right}>
+        <Link href="/login">
+          <button className={styles.loginButton}>LOG IN</button>
+        </Link>
       </div>
     </header>
-  )
+  );
 }

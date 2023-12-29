acbcb566f8cd8f132a86e7ba60a2637e609aa488
Vicki <s6p1j@students.cs.ubc.ca>
2023-12-28 23:40:58 -0800

    login modal additions

--- src/components/login-model.tsx
+++ src/components/login-model.tsx
@@ -6,13 +6,53 @@ interface ModalProps {
   closeModal: () => void;
 }
 
+const inputStyles = {
+    width: "444.757px",
+    height: "62px",
+    color: "black",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    fontSize: "24px",
+    fontFamily: "Inter, sans-serif",
+    borderRadius: "10px",
+
+}
+
 const Modal: React.FC<ModalProps> = ({ showModal, closeModal }) => {
+
+    const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.target === event.currentTarget) {
+          closeModal();
+        }
+      };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // logic TODO
+    };
   return (
     showModal && (
-      <div className={styles.modalBackdrop}>
+      <div className={styles.modalBackdrop} onClick={handleBackdropClick}> 
         <div className={styles.modalContent}>
-          <h2>Modal Content</h2>
-          <button onClick={closeModal}>Close</button>
+          <h1 className={styles.loginTitle}>Login to your account</h1>
+          <form onSubmit={handleSubmit} className={styles.formContainer}>
+            <div className={styles.formGroup}>
+              <input style={inputStyles} type="text" id="username" name="username" placeholder="Username"/>
+            </div>
+            <div>
+                <div className={styles.formGroup}>
+                    <input style={inputStyles} type="password" id="password" name="password" placeholder="Password" />
+                </div>
+                <div className={styles.forgotPassword}>
+                    <a href="#">Forgot your password?</a>
+                </div>
+            </div>
+            
+            <button className={styles.loginButton}>
+                LOG IN
+            </button>
+          </form>
+          <h1 className={styles.signUpText}> Not registered? Sign up.</h1>
         </div>
       </div>
     )

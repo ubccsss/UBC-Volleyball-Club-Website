4d5eb0bd57d4ac2ed660d1ea6fc84fa6c4459801
Vicki <s6p1j@students.cs.ubc.ca>
2024-01-01 15:36:20 -0800

    lint

--- src/components/modal/account-type-button.tsx
+++ src/components/modal/account-type-button.tsx
@@ -33,14 +33,14 @@ const AccountButton: React.FC<AccountButtonProps> = ({
               <path d="M18.7917 20.5028C22.8287 16.7134 28.0965 15.6191 35.875 15.375" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M4.72383 18.8031C8.76677 23.8271 12.9992 27.2148 17.0833 29.0417" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M10.0363 33.8096C17.0931 31.3118 21.0439 25.9984 22.2082 18.7917" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M15.8291 36.8612C24.836 33.7452 29.6539 26.7354 30.75 17.0833" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>       
+              <path d="M15.8291 36.8612C24.836 33.7452 29.6539 26.7354 30.75 17.0833" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         )}
         {accountType === 'admin' && (
           <svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M27.3333 35.875V32.4583C27.3333 30.646 26.6134 28.9079 25.3319 27.6264C24.0504 26.3449 22.3123 25.625 20.5 25.625H10.25C8.43765 25.625 6.69956 26.3449 5.41806 27.6264C4.13656 28.9079 3.41663 30.646 3.41663 32.4583V35.875" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M15.375 18.7917C19.1489 18.7917 22.2083 15.7323 22.2083 11.9583C22.2083 8.18439 19.1489 5.125 15.375 5.125C11.601 5.125 8.54163 8.18439 8.54163 11.9583C8.54163 15.7323 11.601 18.7917 15.375 18.7917Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M27.3334 18.7917L30.75 22.2083L37.5834 15.375" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>  
+            <path d="M27.3334 18.7917L30.75 22.2083L37.5834 15.375" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         )}
         <h1 className={styles['text-basic']}>{accountType === 'player' ? 'Player' : 'Admin'}</h1>

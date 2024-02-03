f44981468ff0e3714293451407d3af8b4eed4cde
JohnnyM21 <johnnymao2005@gmail.com>
2024-02-03 12:06:40 -0800

    Home page edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,39 @@
-import Link from "next/link"
+import Head from 'next/head'
 
-import { siteConfig } from "@/src/config/site"
-import { buttonVariants } from "@/src/components/ui/button"
-
-export default function IndexPage() {
+export default function Home() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+    <div className="bg-white">
+      <Head>
+        <title>UBC Volleyball Site</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="flex justify-between items-center p-4">
+        <img src='/clubLogo.png' alt="Club logo" className="h-12 w-auto" />
+
+        <div className="flex-grow flex justify-end items-center pr-4">
+          <div className="text-center mr-4">
+            <p>CONTACT</p>
+          </div>
+
+          <a href="https://example.com" className="bg-custom-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            LOG IN
+          </a>
+        </div>
+      </header>
+
+      <div className="relative" style={{ marginTop: '5%', marginRight: '10%', marginBottom: '5%', marginLeft: '10%' }}>
+        <img src="/R.jpg" alt="Volleyball image" className="w-full h-auto" />
+        <div className="absolute inset-0 bg-black opacity-30"></div>
+        <div className="absolute inset-0 flex justify-center items-center flex-col text-white">
+          <p className="text-5xl font-bold">
+            UBC Volleyball Club
+          </p>
+          <p className="text-sm mt-2">
+            Playing competitive club volleyball est. 2022
+          </p>
+        </div>
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    </div>
   )
 }

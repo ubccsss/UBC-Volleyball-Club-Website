4417219dabfe22ee3a5d493876d745eb226a4d9f
aneeshb7 <aneesh.bulusu@gmail.com>
2024-02-09 19:31:05 -0800

    make fetch calls with supabase client

--- app/statistics/statistics.ts
+++ app/statistics/statistics.ts
@@ -1,10 +1,15 @@
 import { login } from "../login/login_action"
+import { createClient } from "@/src/utils/supabase/server_action_client"
+import { cookies } from "next/headers"
 
-// boilerplate code for CRUD operations for statistics
+const cookieStore = cookies()
+const supabase = createClient(cookieStore)
+
+// CRUD operations for player stats
 
 // get all stats for a player
 export async function getPlayerStats(playerId: string) {
-  const stats = await fetch(`/api/stats/${playerId}`)
+  const { data: stats, error } = await supabase.from("player_stats").select().eq("player_id", playerId)
   return stats
 }
 
@@ -16,20 +21,16 @@ export async function getTeamStats(teamId: string) {
 
 // get all stats for all players
 export async function getAllStats() {
-  const stats = await fetch(`/api/stats`)
+    const { data: stats, error } = await supabase.from("player_stats").select()
   return stats
 }
 
-// get stats for a particular metric and return in sorted order
+// get stats for a particular metric, sorted in descending order
 export async function getGroupedStats(metric: string) {
-  const stats = await fetch(`/api/stats/${metric}`)
+  const { data: stats, error } = await supabase.from("player_stats").select(metric).order(metric, { ascending: false })
   return stats
 }
 
 export async function updateStats(playerId: string, data: any) {
-  const stats = await fetch(`/api/stats`, {
-    method: "PUT",
-    body: JSON.stringify(data),
-  })
-  return stats
+  const { error } = await supabase.from("player_stats").update(data).eq("player_id", playerId)
 }
